f0032ceadd3e6c86877b58e6faedf0bba64992c3
ljx <ljxing_suda@163.com>
2025-05-11 17:52:37 +0800

    auto: update

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,6 +7,11 @@ const { verifyToken } = require('../middlewares/authMiddleware');
 
 router.post('/login', login);
 
+// 用于验证 JWT Token 是否有效的路由
+router.get('/verifyToken', verifyToken, (req, res) => {
+  res.json({ success: true, message: 'Token 有效' });
+});
+
 // 需要 JWT Token 和 CSRF Token 验证的受保护路由
 router.post('/some-protected-route', verifyToken, verifyCsrfToken, (req, res) => {
   res.json({ success: true, message: '受保护的路由' });
